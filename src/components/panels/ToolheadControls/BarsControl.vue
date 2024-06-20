a9482ecf8b6ca06115d6953e1959c29ecb893b8c
Adam Banicevic <banicevica@seznam.cz>
2024-06-20 15:32:29 +0200

    Light theme support and hide some things

--- src/components/panels/ToolheadControls/BarsControl.vue
+++ src/components/panels/ToolheadControls/BarsControl.vue
@@ -54,7 +54,7 @@
             </v-col>
         </v-row>
         <!-- X MOVEMENT BUTTONGROUPS -->
-        <v-row dense>
+        <v-row v-if="AdvancedFeatures" dense>
             <v-col class="text-center">
                 <v-item-group class="_btn-group row no-gutters">
                     <v-btn
@@ -85,7 +85,7 @@
             </v-col>
         </v-row>
         <!-- Y MOVEMENT BUTTONGROUPS -->
-        <v-row dense>
+        <v-row v-if="AdvancedFeatures" dense>
             <v-col class="text-center">
                 <v-item-group class="_btn-group row no-gutters">
                     <v-btn
@@ -116,7 +116,7 @@
             </v-col>
         </v-row>
         <!-- Z MOVEMENT BUTTONGROUPS -->
-        <v-row dense>
+        <v-row v-if="AdvancedFeatures" dense>
             <v-col class="text-center">
                 <v-item-group class="_btn-group row no-gutters">
                     <v-btn
@@ -152,11 +152,12 @@
 <script lang="ts">
 import { Component, Mixins } from 'vue-property-decorator'
 import BaseMixin from '@/components/mixins/base'
+import TrilabMixin from '@/components/mixins/trilab'
 import ControlMixin from '@/components/mixins/control'
 import { mdiEngineOff, mdiHome } from '@mdi/js'
 
 @Component
-export default class BarsControl extends Mixins(BaseMixin, ControlMixin) {
+export default class BarsControl extends Mixins(BaseMixin, ControlMixin, TrilabMixin) {
     mdiEngineOff = mdiEngineOff
     mdiHome = mdiHome
 
