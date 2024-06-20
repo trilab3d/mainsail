4a7742d61f83c3bc3877030cb036159eca0acdcd
Adam Banicevic <banicevica@seznam.cz>
2024-06-20 09:35:24 +0200

    webkamera a fix prekladu

--- src/store/gui/webcams/getters.ts
+++ src/store/gui/webcams/getters.ts
@@ -1,10 +1,28 @@
 import { GetterTree } from 'vuex'
 import { GuiWebcamState, GuiWebcamStateWebcam } from '@/store/gui/webcams/types'
+import { mdiWebcam } from '@mdi/js'
 
 // eslint-disable-next-line
 export const getters: GetterTree<GuiWebcamState, any> = {
     getWebcams: (state) => {
-        return state.webcams.filter((webcam: GuiWebcamStateWebcam) => webcam.enabled)
+        const webcams = state.webcams.filter((webcam: GuiWebcamStateWebcam) => webcam.enabled)
+        /// check if webcams is empty array and if so, create a default webcam
+        if (webcams.length === 0) {
+            webcams.push({
+                name: 'Default webcam',
+                icon: mdiWebcam,
+                service: 'mjpegstreamer-adaptive',
+                target_fps: 15,
+                stream_url: '/webcam/?action=stream',
+                snapshot_url: '/webcam/?action=snapshot',
+                rotation: 0,
+                flip_horizontal: false,
+                flip_vertical: false,
+                enabled: true,                
+            })
+        }
+
+        return webcams
     },
 
     getWebcam: (_, getters) => (name: string) => {
